e3d525e633dc2a45ad12b038f73b664e11433ca3
alifprihantoro <alifprihantoro@gmail.com>
2024-10-21 20:21:10 +0700

    feat: add pagenation

---
+++ pkgs/ui/src/view/components/pagenation/NextPrev.ts
@@ -0,0 +1,14 @@
+export default function NextPrev() {
+  return html`
+    <div class="NextPrev">
+      <a href="#">
+        <h2>Title</h2>
+        <span>Previous News</span>
+      </a>
+      <a href="#">
+        <h2>Title</h2>
+        <span>Next News</span>
+      </a>
+    </div>
+  `
+}
---
+++ pkgs/ui/src/view/components/pagenation/Pagenation.stories.ts
@@ -0,0 +1,10 @@
+import NextPrev from './NextPrev'
+import LoadMore from './LoadMore'
+
+export default {}
+export const NextPrevPages = {
+  render: NextPrev,
+}
+export const LoadMoreBtn = {
+  render: LoadMore,
+}
