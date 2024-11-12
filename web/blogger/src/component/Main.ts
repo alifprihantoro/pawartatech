4efc460ed9c0bb26f5df71ece6ecec3dbccb7b63
alifprihantoro <alifprihantoro@gmail.com>
2024-11-12 08:47:12 +0700

    feat: add tag fonts

--- web/blogger/src/component/Main.ts
+++ web/blogger/src/component/Main.ts
@@ -20,6 +20,14 @@ export default html`
       <meta http-equiv="X-UA-Compatible" content="IE=edge" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       ${seo}
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap"
+        rel="stylesheet" />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Rokkitt:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet" />
       <b:skin> <![CDATA[ ${myCss} ]]> </b:skin>
       &lt;!--</head
     >--&gt;&lt;/head&gt; &lt;!--
