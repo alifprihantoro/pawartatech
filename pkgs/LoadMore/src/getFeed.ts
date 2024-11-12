cb3d5f29db772b35ffc7209f5a6078203984f46b
alifprihantoro <alifprihantoro@gmail.com>
2024-11-12 08:46:51 +0700

    fix: loadmore url

--- pkgs/LoadMore/src/getFeed.ts
+++ pkgs/LoadMore/src/getFeed.ts
@@ -7,13 +7,14 @@ export default async function getFeed(PAGE_NUM: number) {
     }
     return ''
   }
+
+  const getUrl = window.location.origin
   const MAX = 5
-  const BLOG_NAME = 'pawartatech'
   const ORDER_BY = 'updated' // updated, starttime
   const START_INDEX = PAGE_NUM
   // const SEARCH = '&q='
   const LABEL = getLabelFromUrl()
-  const URL_FEED = `https://${BLOG_NAME}.blogspot.com/feeds/posts/summary?alt=json&max-results=${MAX}&start-index=${START_INDEX}&orderby=${ORDER_BY}${LABEL}`
+  const URL_FEED = `${getUrl}/feeds/posts/summary?alt=json&max-results=${MAX}&start-index=${START_INDEX}&orderby=${ORDER_BY}${LABEL}`
   const CONTENT = await fetch(URL_FEED).then((res) => res.json())
 
   const FEED = CONTENT.feed
