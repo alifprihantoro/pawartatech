43e93c6f9b84b7272a5392e276b306ec50086422
alifprihantoro <alifprihantoro@gmail.com>
2024-11-22 09:07:03 +0700

    feat: no images

--- pkgs/ui/src/view/components/Card/index.ts
+++ pkgs/ui/src/view/components/Card/index.ts
@@ -8,8 +8,8 @@ export default function Card({ title, img, spoiler, link }: TCardArgs) {
   const DEFAULT_IMG = ''
   const IMG =
     img || DEFAULT_IMG
-      ? `<img loading="lazy" class="min-w-full w-80" src="${img || DEFAULT_IMG}" alt="Album" />`
-      : ''
+      ? `<img loading="lazy" class="max-w-full w-80" src="${img || DEFAULT_IMG}" alt="Album" />`
+      : '<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 32 32"><path fill="white" d="M30 3.414L28.586 2L2 28.586L3.414 30l2-2H26a2.003 2.003 0 0 0 2-2V5.414zM26 26H7.414l7.793-7.793l2.379 2.379a2 2 0 0 0 2.828 0L22 19l4 3.997zm0-5.832l-2.586-2.586a2 2 0 0 0-2.828 0L19 19.168l-2.377-2.377L26 7.414zM6 22v-3l5-4.997l1.373 1.374l1.416-1.416l-1.375-1.375a2 2 0 0 0-2.828 0L6 16.172V6h16V4H6a2 2 0 0 0-2 2v16z"/></svg>'
 
   return html`
     <div class="card lg:card-side bg-base-100 shadow-xl my-6 mx-3">
--- web/blogger/src/component/pages/List.ts
+++ web/blogger/src/component/pages/List.ts
@@ -4,8 +4,7 @@ import Loading from 'ui/src/view/components/Loading'
 export default html`
   <div id="LoadMoreList">${Loading(5)}</div>
   <div id="LoadMoreBtn" class="w-full mb-6">
-    <button class="btn bg-base-300 block m-auto">
-      Loading
+    <button class="btn btn-disabled block m-auto">
       <span class="loading loading-spinner loading-xs"></span>
     </button>
   </div>
